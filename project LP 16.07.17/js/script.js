175192f2e39b8f79d74b6757e98f59026a511439
Leshka.Grbnshikov <barada4@mail.ru>
2017-07-24 23:48:24 +0500

    ...

---
+++ project LP 16.07.17/js/script.js
@@ -0,0 +1,21 @@
+﻿getElementById('outPutBlock').style.heigt = '300px';
+
+
+
+
+/*
+function onClick(){
+  document.getElementById('#FindAGig').onclick =getListItem();
+}
+
+
+function getListItem(){
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200) {
+      document.getElementbyId("outPutBlock").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "http://demos.wonderkidstudio.com/sites/crud/api/index.php?/api", true);
+  xhttp.send();
+};
