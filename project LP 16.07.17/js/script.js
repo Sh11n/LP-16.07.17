a5c3a7de48b94a383c0b7b3b65dbf26384ceea00
Leshka.Grbnshikov <barada4@mail.ru>
2017-07-27 11:25:16 +0500

    разбор с кнопкой, несовсем понятно как  гет запрос получить если сам сайт распологается в др месте

--- project LP 16.07.17/js/script.js
+++ project LP 16.07.17/js/script.js
@@ -1,21 +1,43 @@
-﻿getElementById('outPutBlock').style.heigt = '300px';
-
-
-
-
-/*
-function onClick(){
-  document.getElementById('#FindAGig').onclick =getListItem();
-}
-
-
-function getListItem(){
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200) {
-      document.getElementbyId("outPutBlock").innerHTML = this.responseText;
-    }
+﻿/*задача
+при клике по кнопке, создать див в определенной области
+*/
+var button1 = document.getElementById('FindAGig');
+    
+    button1.addEventListener('click', show, loadDoc());
+    
+  function addElem(){
+    var find = document.getElementById('outPutBlock');
+    var div = document.createElement('div');
+    
+    div.style.backgroundColor = "white";
+    div.style.width = "98%";
+    div.style.marginLeft = "2%";
+    div.style.height = "60px";
+    div.style.display = "none";
+    div.id = "divHide";
+    find.appendChild(div);
   };
-  xhttp.open("GET", "http://demos.wonderkidstudio.com/sites/crud/api/index.php?/api", true);
-  xhttp.send();
-};
+  
+  function show(){
+    var div = document.getElementById('divHide');
+    div.style.display = "block";
+  };
+ 
+  function loadDoc(){
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+        document.getElementById('divHide').innerHTML = this.responseText;
+      }
+    };
+    xhttp.open("GET", "http://demos.wonderkidstudio.com/sites/crud/api/index.php?/api?/products/id30", true);
+    xhttp.send();
+  };
+  
+  addElem();
+  
+  /*
+  слушатель событий  оповещает о том что данные поступили и готовы
+  xhttp.addEventListener("load", function() {
+  console.log("Done:", req.status);
+  */
