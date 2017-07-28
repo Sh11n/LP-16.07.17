907675ca366cc26b367bcc81ee243e6793af87de
Leshka.Grbnshikov <barada4@mail.ru>
2017-07-28 17:59:37 +0500

    на жс создал блок, и сделал при нажатии на блок с веревочками  появление и скрытие созданного блока в который выводится информация с запроса на сервер. пока что выводится только ошибка соединения насколько я понял заголовок сервера тобишь Origin является пустым и поэтому кросс доменный запрос ГЕТ не выполняется и выдает ошибку подключение к серверу. а может я перепутал что то в урле или эе не дописал в коде пока что не смог разобраться

--- project LP 16.07.17/js/script.js
+++ project LP 16.07.17/js/script.js
@@ -1,43 +1,86 @@
 ﻿/*задача
 при клике по кнопке, создать див в определенной области
 */
-var button1 = document.getElementById('FindAGig');
-    
-    button1.addEventListener('click', show, loadDoc());
-    
-  function addElem(){
+function addElem(){ //add hide blcok
     var find = document.getElementById('outPutBlock');
     var div = document.createElement('div');
-    
+    var p = document.createElement('p');
     div.style.backgroundColor = "white";
     div.style.width = "98%";
     div.style.marginLeft = "2%";
+    div.style.marginTop = "2%";
     div.style.height = "60px";
+    div.style.padding = "10px";
     div.style.display = "none";
     div.id = "divHide";
+    p.id = "pHide";
+    div.appendChild(p);
     find.appendChild(div);
   };
   
-  function show(){
-    var div = document.getElementById('divHide');
-    div.style.display = "block";
-  };
+  addElem();
+  
+//onclick load doc 
+document.getElementById('FindAGig').addEventListener('click', loadDoc);
  
-  function loadDoc(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-      if(this.readyState == 4 && this.status == 200){
-        document.getElementById('divHide').innerHTML = this.responseText;
-      }
-    };
-    xhttp.open("GET", "http://demos.wonderkidstudio.com/sites/crud/api/index.php?/api?/products/id30", true);
+  function loadDoc(){ //xmlhttpreauest, get to serv
+    var XHTTP = ("onload" in new XMLHttpRequest())? XMLHttpRequest : XDomainRequest;
+    var xhttp = new XHTTP(); 
+    
+    xhttp.open("GET", "http://demos.wonderkidstudio.com/sites/crud/api/index.php?/api", true);
+    xhttp.onload = function(){
+      document.getElementById('pHide').innerHTML = JSON.parse(this.responseText);
+    }
+    xhttp.onerror = function(){
+       document.getElementById('pHide').innerHTML = 'Ошибка ' + this.status;
+    }
     xhttp.send();
   };
   
-  addElem();
+  
+  //onclick hide block
+  document.getElementById('outPutBlock').addEventListener('click', function(){
+    var div = document.getElementById('divHide');
+    var dStyle = div.style;
+    
+    if(dStyle.display == 'block'){
+      dStyle.display = 'none';
+    }else{
+      dStyle.display = 'block';
+    };
+  });
+  
   
   /*
   слушатель событий  оповещает о том что данные поступили и готовы
   xhttp.addEventListener("load", function() {
   console.log("Done:", req.status);
   */
+   
+    /*  
+  function addElem(){ //добавляет блок на страницу
+    var find = document.getElementById('outPutBlock');
+    var div = document.createElement('div');
+    div.style.backgroundColor = "white";
+    div.style.width = "98%";
+    div.style.marginLeft = "2%";
+    div.style.marginTop = "2%";
+    div.style.height = "60px";
+    //div.style.display = "none";
+    div.id = "divHide";
+    find.appendChild(div);
+  };
+  
+  addElem();
+   */
+   /*
+   function loadDoc(){ //гет запрос на серв но не настроен
+    var XHTTP = ("onload" in new XMLHttpRequest())? XMLHttpRequest : XDomainRequest;
+    var xhttp = new XHTTP; 
+    xhttp.onreadystatechange = function(){
+      if(this.readyState == 4 && this.status == 200){
+        document.getElementById('divHide').innerHTML = JSON.parse(this.responseText);
+      }
+    };
+   */
+    
